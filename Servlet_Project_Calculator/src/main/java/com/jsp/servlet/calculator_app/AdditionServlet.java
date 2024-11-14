0517c479eed561491f3a22f492aa7de670c317a9
pnadka <nadkaprahalad@gmail.com>
2024-11-14 11:20:32 +0530

    fourth committ

---
+++ Servlet_Project_Calculator/src/main/java/com/jsp/servlet/calculator_app/AdditionServlet.java
@@ -0,0 +1,26 @@
+package com.jsp.servlet.calculator_app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class AdditionServlet extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		String n1 =req.getParameter("num1");
+		int num1=Integer.parseInt(n1);
+		int num2=Integer.parseInt(req.getParameter("num2"));
+		
+		int sum=num1+num2;
+		
+		PrintWriter out=res.getWriter();
+		out.print("<h1>The sum of the numbers is:"+sum+"</h1>");
+	}
+	
+
+}
