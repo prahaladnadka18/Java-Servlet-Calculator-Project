2ccc5a4983524920e4a071dff216fda878706a70
pnadka <nadkaprahalad@gmail.com>
2024-11-14 11:19:44 +0530

    second committ

---
+++ Servlet_Project_Calculator/src/main/java/com/jsp/servlet/calculator_app/SubtractionServlet.java
@@ -0,0 +1,23 @@
+package com.jsp.servlet.calculator_app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SubtractionServlet extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		int num1=Integer.parseInt(req.getParameter("num1"));
+		int num2=Integer.parseInt(req.getParameter("num2"));
+		int sub=num1-num2;
+
+		PrintWriter out = res.getWriter();
+		out.print("<h1> Subraction of Numbers is: "+sub+"</h1>");
+
+	}
+}
